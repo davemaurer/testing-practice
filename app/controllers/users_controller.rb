0452f4914befca0af2e3e64a00164ea25878e34d
David Maurer <davemaurermail@gmail.com>
2017-03-18 23:28:20 -0600

    Add users folder to app/views along with index erb file. Define set_user method in UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
-  before_action set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
